502d80697ddeb41a92f2a4d3b9da2b615173484f
furxx2000 <furxx2000@gmail.com>
2022-03-22 23:01:46 +0800

    新增BasePasswordCheck component

---
+++ src/components/UI/BasePasswordCheck.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="check_badge">
+    <svg-icon
+      :icon-class="icon"
+      :class="[{ isActive: isLengthActive || isOneNumberActive }, smallIcon]"
+    ></svg-icon>
+    <span
+      class="check-badge__text"
+      :class="{ isActive: isLengthActive || isOneNumberActive }"
+    >
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    isLengthActive: {
+      type: Boolean,
+      default: false,
+    },
+    isOneNumberActive: {
+      type: Boolean,
+      default: false,
+    },
+    isSmall: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const smallIcon = computed(() => {
+      if (props.isSmall) return "small";
+    });
+
+    return { smallIcon };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.svg-icon.small {
+  width: 14px;
+  height: 14px;
+  fill: #ababab;
+  margin-right: 1px;
+
+  &.isActive {
+    fill: #4ae7a5;
+  }
+}
+
+.check-badge__text {
+  color: #ababab;
+  font-size: 12px;
+  font-weight: 500;
+
+  &.isActive {
+    color: black;
+  }
+}
+</style>
