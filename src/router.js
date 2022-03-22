50f2bbdf21e5ed2ed5bbef9993367656cb22c70e
furxx2000 <furxx2000@gmail.com>
2022-03-22 22:57:55 +0800

    新增Router讓button連結到登入頁面

--- src/router.js
+++ src/router.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import UserAuth from "./components/Page/UserAuth.vue";
+import Login from "./components/Page/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/login" },
-    { path: "/login", component: UserAuth },
+    { path: "/login", component: Login },
+    {
+      path: "/login/google",
+      beforeEnter() {
+        window.location.href = "https://accounts.google.com/";
+      },
+    },
+    {
+      path: "/login/facebook",
+      beforeEnter() {
+        window.location.href = "https://zh-tw.facebook.com/";
+      },
+    },
   ],
 });
 
