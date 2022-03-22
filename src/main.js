c8e853c50f911913a1ab3d2121642d390f0cf347
furxx2000 <furxx2000@gmail.com>
2022-03-22 22:57:07 +0800

    新增component

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 import BaseCard from "./components/UI/BaseCard.vue";
 import BaseButton from "./components/UI/BaseButton.vue";
-import BaseCheck from "./components/UI/BaseCheck.vue";
+import BasePasswordCheck from "./components/UI/BasePasswordCheck.vue";
+import BaseInput from "./components/UI/BaseInput.vue";
 import SvgIcon from "./components/UI/SvgIcon.vue";
 
 import "normalize.css";
@@ -15,7 +16,8 @@ app.use(router);
 
 app.component("base-card", BaseCard);
 app.component("base-button", BaseButton);
-app.component("base-check", BaseCheck);
+app.component("base-passwordCheck", BasePasswordCheck);
+app.component("base-input", BaseInput);
 app.component("svg-icon", SvgIcon);
 
 const requireAll = (requireContext) =>
