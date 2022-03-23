407730cd69c82089b24b389596b6f8e5f90978d4
furxx2000 <furxx2000@gmail.com>
2022-03-23 12:54:42 +0800

    新增加router path

--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "./components/Page/Login.vue";
+import LoggedInSuccessfully from "./components/Page/LoggedInSuccessfully.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/login" },
-    { path: "/login", component: Login },
+    {
+      path: "/login",
+      component: Login,
+    },
+    { path: "/loggedInSuccessfully", component: LoggedInSuccessfully },
     {
       path: "/login/google",
       beforeEnter() {
