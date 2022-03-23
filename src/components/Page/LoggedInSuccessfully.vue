c007e81e45490fb4e444830190014f19e4643b61
furxx2000 <furxx2000@gmail.com>
2022-03-23 12:56:23 +0800

    新增加LoggedInSuccessfully.vue page

---
+++ src/components/Page/LoggedInSuccessfully.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-card>
+    <the-previous-page-button>Back</the-previous-page-button>
+    <h2>Creating account successfully</h2>
+    <h3>Thank you</h3>
+  </base-card>
+</template>
+
+<script>
+import ThePreviousPageButton from "../Layout/ThePreviousPageButton.vue";
+
+export default {
+  components: { ThePreviousPageButton },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+h2,
+h3 {
+  text-align: center;
+}
+</style>
