d4fb3fcb4d5070afc9f0f9cb4d8734dc01e20e63
furxx2000 <furxx2000@gmail.com>
2022-03-23 12:49:48 +0800

    修改transition name, 縮短漸變時間

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
+    <transition name="router" mode="out-in">
       <component :is="Component" :key="$route.path"></component>
     </transition>
   </router-view>
@@ -21,26 +21,26 @@ body {
   background-color: #f1f5ff;
 }
 
-.fade-enter-from {
+.router-enter-from {
   opacity: 0;
   transform: translateY(-30px);
 }
 
-.fade-leave-to {
+.router-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
 
-.fade-enter-active {
-  transition: all 0.8s ease-out;
+.router-enter-active {
+  transition: all 0.5s ease-out;
 }
 
-.fade-leave-active {
-  transition: all 0.8s ease-in;
+.router-leave-active {
+  transition: all 0.5s ease-in;
 }
 
-.fade-enter-to,
-.fade-leave-from {
+.router-enter-to,
+.router-leave-from {
   opacity: 1;
   transform: translateY(0);
 }
