99445212ef96301e151b80bf463cbe90e9d6082c
furxx2000 <furxx2000@gmail.com>
2023-02-20 22:35:41 +0800

    Add publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,23 @@
-const path = require("path");
+const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, "./", dir);
+  return path.join(__dirname, './', dir);
 }
 
 module.exports = {
+  publicPath: '/Login-page/',
   chainWebpack: (config) => {
     // 先刪除預設的svg配置
-    config.module.rule("svg").exclude.add(resolve("src/assets/icon")).end();
+    config.module.rule('svg').exclude.add(resolve('src/assets/icon')).end();
 
     // 新增 svg-sprite-loader 設定
     config.module
-      .rule("icons")
+      .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve("src/assets/icon"))
+      .include.add(resolve('src/assets/icon'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
-      .options({ symbolId: "[name]" });
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({ symbolId: '[name]' });
   },
 };
